2cb475a4b7cd40f8ccb5ab1296a3166f48ba6358
97mams <97mams@gmail.com>
2025-04-07 08:06:33 -0700

    feat: setting

---
+++ src/vue/page/setting.php
@@ -0,0 +1,86 @@
+<?php
+
+$title = 'reglage';
+
+ob_start();
+?>
+
+<div class="mt-8 my-6 w-full overflow-y-auto">
+  <h1 class="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Page de réglages</h1>
+  <p class="leading-7 [&:not(:first-child)]:mt-6">Vous pouvez ajouter ici tous les vaccins conserner à la traitement de vos animalaux.</p>
+  <div>
+    <h3 class="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">Formulaire du type d'élevage</h3>
+    <form action="" method="post">
+      <div>
+        <label for="type_animal" class= "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Type d'élevage:</label>
+        <input 
+          type="text" 
+          name="type_animal" 
+          id="type_animal"
+          required 
+          class=" w-sm flex h-9 w-full rounded-md border border-[ #e6e6e6] bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-[oklch(0.145 0 0)] placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[oklch(0.708 0 0)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          >
+      </div>
+      <button type="submit" class="mt-8 h-9 px-4 py-2 bg-[#171717] cursor-pointer text-[#fafafa] shadow-sm hover:bg-gray-700 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#a1a1a1] disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 [&_svg]:shrink-0">
+        Ajouter
+      </button>
+    </form>
+  </div>
+
+  <div>
+    <h3 class="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">Formulaire du traitement</h3>
+    <form action="" method="post">
+      <div>
+        <label for="type" class= "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Ajouter le type de traitement(ex: vaccination ou pile):</label>
+        <input 
+          type="text" 
+          name="type" 
+          id="type"
+          required 
+          class="flex h-9 w-full rounded-md border border-[ #e6e6e6] bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-[oklch(0.145 0 0)] placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[oklch(0.708 0 0)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          >
+      </div>
+      <div>
+        <label for="description" class= "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Ajouter la description:</label>
+        <input 
+          type="text" 
+          name="description" 
+          id="description"
+          required 
+          class="flex h-9 w-full rounded-md border border-[ #e6e6e6] bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-[oklch(0.145 0 0)] placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[oklch(0.708 0 0)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          >
+      </div>
+      <div>
+        <label for="date" class= "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Ajouter la durration de rappel (ex: tout le 30 jours):</label>
+        <input 
+          type="text" 
+          name="date" 
+          id="date"
+          required 
+          class=" w-sm flex h-9 w-full rounded-md border border-[ #e6e6e6] bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-[oklch(0.145 0 0)] placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[oklch(0.708 0 0)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          >
+      </div>
+      <button type="submit" class="mt-8 h-9 px-4 py-2 bg-[#171717] cursor-pointer text-[#fafafa] shadow-sm hover:bg-gray-700 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#a1a1a1] disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 [&_svg]:shrink-0">
+        Ajouter
+      </button>
+    </form>
+  </div>
+  <div class="w-full">
+      <h3 class="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">Liste de reglages</h3>
+      <table class="w-full border border-[#171717]">
+        <thead>
+          <tr>
+            <th class="border border-[#171717]">Vaccin</th>
+            <th class="border border-[#171717]">Description</th>
+            <th class="border border-[#171717]">Date</th>
+            <th class="border border-[#171717]">Elevages</th>
+          </tr>
+        </thead>
+      </table>
+  </div>
+</div>
+
+<?php
+$content = ob_get_clean();
+
+include '../src/vue/layouts/base.php';
