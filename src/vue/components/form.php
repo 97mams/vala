70a533d606732486dece058fb7c3d52515fcedf6
97mams <97mams@gmail.com>
2025-04-06 22:45:27 -0700

    add component form

---
+++ src/vue/components/form.php
@@ -0,0 +1,48 @@
+<?php
+$action = "";
+if(empty($animal['id'])) {
+  $action = "/store";
+} else {
+  $action = "/update";
+}
+?>
+
+<div class="w-full">
+<h1 class="text-xl font-bold text-gray-400 my-8">Page d"ajout</h1>
+<div class="w-full flex gap-2">
+  <form action="<?=$action?>" method="post" class="flex flex-col gap-2">
+   <div class="flex flex-col">
+    <label for="name">Nom</label>
+    <input type="text" name="name" id="name" class="border border-gray-400 rounded px-2 py-2" value="<?=$animal["name"]?>">
+   </div>
+   <div class="flex  flex-col">
+     <label for="genre">Genre</label>
+     <input type="text" name="genre" id="genre" class="border border-gray-400 rounded   px-2 py-2" value="<?=$animal["genre"]?>">
+   </div>
+
+   <div  class="flex flex-col">
+     <label for="type">Type</label> 
+     <input type="text" name="type" id="type" class="border border-gray-400 rounded  px-2 py-2" value="<?=$animal["type"]?>">
+   </div>
+
+   <div  class="flex flex-col">
+     <label for="age">Age</label> 
+     <input type="text" name="age" id="age" class="border border-gray-400 rounded  px-2 py-2" value="<?=$animal["age"]?>">
+   </div>
+  <?php
+    if ($action == '/store') {
+      echo '
+      <div  class="flex flex-col">
+        <label for="sexe-animal">Sexe</label>
+        <select name="sexe" id="sexe-animal" class="border border-gray-400 rounded  px-2 py-2">
+          <option value="male">male</option>
+          <option value="female">femmele</option>
+        </select>   
+      </div>';
+    } 
+  ?>
+      
+    <input type="submit" value="Enregistrer" class="bg-blue-500 py-2 px-2 my-8 rounded text-white cursor-pointer">
+  </form>
+</div>
+</div>";
