43f0c5bf3952e314c6781864b879503f8376d4fe
97mams <97mams@gmail.com>
2025-04-09 08:55:28 -0700

    changed name class

---
+++ src/model/TypeBreedModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\model;
+
+use App\config\ConnextionBdd;
+
+class TypeBreedgeModel
+{
+
+  public function index()
+  {
+    try{
+      $db = ConnextionBdd::connect();
+      $types = $db->query("SELECT * FROM type_elevage");
+     return $types->fetchAll();
+    } catch (\PDOException $th) {
+      throw $th;
+    } 
+  }
+
+  public function store($request) 
+  {
+    $name = $request->get('type_animal');
+    try {
+      $db = ConnextionBdd::connect();
+      $query = $db->query('INSERT INTO type_elevage (nom_type) VALUES ("'.$name.'")');
+      
+      return true;
+    } catch (\PDOException $th) {
+      throw $th;
+    }
+  }
+
+}
