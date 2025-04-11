4f4f1f1908fd3ee511f0e6922bec669b018420ef
97mams <97mams@gmail.com>
2025-04-11 10:55:26 -0700

    fix: doc and query

--- src/model/TreatModel.php
+++ src/model/TreatModel.php
@@ -22,19 +22,25 @@ class TreatModel
   {
     try {
       $db = ConnextionBdd::connect();
-      $query = $db->query("SELECT * FORM traiter WHERE id_animale=".$idAnimal);
+      $query = $db->query("SELECT * FROM traiter JOIN traitement on traiter.id_traitement=traitement.id_traitement  WHERE traiter.id_animale=".$idAnimal);
       return $query->fetchAll();
     } catch (\PDOException $th) {
       throw new Error($th);
     }
   }
 
-  public function store($aniaml, $treatment) : void
+  /**
+   * 
+   * @param int $animal
+   * @param int $treatment
+   * 
+   */
+  public function store($animal, $treatment) : void
   {
     $status = 0;
     try {
       $db = ConnextionBdd::connect();
-      $db->query("INSERT INTO traiter (id_animale, id_traitement,status) VALUES(".(int)$aniaml.", ".(int)$treatment.", ".$status.")");
+      $db->query("INSERT INTO traiter (id_animale, id_traitement,status) VALUES(".(int)$animal.", ".(int)$treatment.", ".$status.")");
     } catch (\PDOException $th) {
       throw new Error($th);
     }
