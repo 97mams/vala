49b06f4e4f041f0700f31f9e806f06060d0404ef
97mams <97mams@gmail.com>
2025-04-11 22:59:16 -0700

    fix: td action

--- src/vue/page/index.php
+++ src/vue/page/index.php
@@ -29,7 +29,7 @@ ob_start();
             <?php
           foreach ($animals as $animal) {
           echo '
-          <tr class="m-0 border-t p-0 even:bg-[oklch(0.97 0 0)]"> 
+          <tr class="m-0  p-0 even:bg-[oklch(0.97 0 0)]"> 
             <td  class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
               <a href="/animal/'.$animal["id_animale"].'">
                 '.$animal['nom_animale'].'
@@ -50,12 +50,14 @@ ob_start();
                 '.$animal["age"].'
               </a>
             </td >
-            <td class="border text-left [&[align=center]]:text-center [&[align=right]]:text-right flex p-2 gap-2 "> 
-            <a href="/edit/animal/'.$animal["id_animale"].'" class="px-2 bg-orange-400 text-orange-200 cursor-pointer rounded">✔</a>
-             <form action="/animal"  method="post">
-                <input type="hidden" value="'.$animal["id_animale"].'" name="id">
-                <input title="supprimer" type="submit" value="❌" class="px-2 bg-red-400 text-red-200 rounded cursor-pointer">
-              </form>
+            <td class="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right"> 
+              <div class="flex gap-2">
+                <a href="/edit/animal/'.$animal["id_animale"].'" title="modifier" class="hover:bg-orange-400/80 px-2 bg-orange-400 text-orange-200 cursor-pointer rounded">✔</a>
+                <form action="/animal"  method="post">
+                  <input type="hidden" value="'.$animal["id_animale"].'" name="id">
+                  <input title="supprimer" type="submit" value="❌" class="px-2 bg-red-400 hover:bg-red-400/80 text-red-200 rounded cursor-pointer">
+                </form>
+              </div>
              </td>
             </tr>';
           }
