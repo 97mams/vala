ff7a7dbf90a7c2985f90e6394c0ea1279a49c956
97mams <97mams@gmail.com>
2025-04-17 20:54:08 -0700

    add success message

--- src/vue/page/edit.php
+++ src/vue/page/edit.php
@@ -1,6 +1,14 @@
 <?php
 $title = "register";
 
+if (isset($_GET['message']) && $_GET['message'] === 1) {
+  echo ' 
+  <div class="bg-blue-100 border border-bleu-400 rounded py-8 px-4 mt-8">
+    <p class="text-bleu-400 leading-7 [&:not(:first-child)]:mt-6">Modification réussi !</p>
+  </div>
+  ';
+}
+
 ob_start();
 include('../src/vue/components/form.php');
 $content = ob_get_clean();
