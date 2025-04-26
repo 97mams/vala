194c1898eddf6d62b2a9ceeb0b0ecd66f0dc2286
97mams <97mams@gmail.com>
2025-04-25 20:27:45 -0700

    add endpoint seed

--- bin/app.php
+++ bin/app.php
@@ -2,16 +2,18 @@
 <?php
 
 use App\CLI\Db;
+use App\seed\GenreSeed;
 
 require "./vendor/autoload.php";
 
 $database = new Db();
-
+$seed = new GenreSeed();
 $command = $argv[1] ?? null;
 $arg1    = $argv[2] ?? null;
 
 match ($command) {
   "db:create" => $database->createDatabase(),
   "db:table" => $database->createTable($arg1),
-   default => print "command is not matching"
+  "seed:run" => $seed->seed(),
+  default => print "command is not matching"
 };
