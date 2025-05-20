95b8eb5dd74dbef89dabdb7fc0fa4f90e1da0705
97mams <97mams@gmail.com>
2025-05-20 10:03:44 -0700

    checked type

--- src/Error/HandelError.php
+++ src/Error/HandelError.php
@@ -4,7 +4,22 @@ namespace App\Error;
 
 use Error;
 
+/**
+ * catch all error
+ */
 class HandelError extends Error
 {
-  //
-}
+  public static function get(Object $error):string
+  {
+    $result = "";
+    if($error instanceof \PDOException){
+      $result = '
+      <div class="bg-red-100 border border-red-400 rounded py-8 px-4 mt-8">
+        <p class="text-red-400 leading-7 [&:not(:first-child)]:mt-6">Remplir les champs dans vos réglages.</p>
+      </div>';
+    }
+
+    return $result;
+  }
+
+} 
