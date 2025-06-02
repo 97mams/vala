4d4ace243366d3731a42d8f3435d4bb95bb74958
97mams <97mams@gmail.com>
2025-06-02 07:44:46 -0700

    add notification register

--- src/controller/NotificationController.php
+++ src/controller/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\controller;
 
 use App\model\NotificationModel;
+use Symfony\Component\HttpFoundation\Request;
 
 class NotificationController
 {
@@ -20,6 +21,16 @@ class NotificationController
   public function index():array
   {
     return $this->model->index();
+  }
+
+  /**
+   * notification register
+   * @param Request $request
+   * @return void
+   */
+  public function store(Request $request): void
+  {
+    $this->model->store($request);
   }
 
 }
--- src/model/NotificationModel.php
+++ src/model/NotificationModel.php
@@ -3,6 +3,7 @@
 namespace App\model;
 
 use App\config\ConnextionBdd;
+use Symfony\Component\HttpFoundation\Request;
 
 class NotificationModel
 {
@@ -18,5 +19,15 @@ class NotificationModel
     $query = $this->connectDb
                   ->query("SELECT * FROM notification");
     return $query->fetchAll();
+  }
+
+  public function store(Request $request):void
+  {
+    $name       = $request->get('name');
+    $description= $request->get('description');
+    $status     = $request->get('status');
+    $date       = date("y-m-d H:m:s");
+    $this->connectDb
+         ->query('INSERT INTO notification (nom, description, status, created_at, updated_at) VALUES("'.$name.'", "'.$description.'" ,"'.$status.'", "'.$date.'", "'.$date.'")');
   }
 }
