49a3c7ae8357c79f884e83ba2dc2905857fa10aa
97mams <97mams@gmail.com>
2025-06-26 06:50:37 -0700

    add story controller

---
+++ src/controller/StoryCotroller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\controller;
+
+use App\model\StoryModel;
+
+class StoryCotroller
+{
+  private $modelSotry;
+
+  public function __construct()
+  {
+    $this->modelSotry = new StoryModel();
+  }
+
+
+  public function index(string $nameAnimal):array
+  {
+    $model = $this->modelSotry;
+    return $model->getStory($nameAnimal);
+  }
+
+}
