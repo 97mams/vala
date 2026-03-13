06b61f4c63dfcf939526e9dda2b121017e9f54f3
97mams <97mams@gmail.com>
2026-03-13 18:24:39 +0300

    fix: add id to submit button for treatment form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 const btnNotification = document.getElementById("btn-notification");
 const btnBreed = document.getElementById("btn-breed");
+const btnTrait = document.getElementById("btn-trait");
 const cardNotification = document.getElementById("notification");
 const input = document.getElementsByTagName("input");
 
@@ -7,14 +8,13 @@ btnNotification.addEventListener("click", (e) => {
   cardNotification.classList.toggle("hidden");
 });
 
-btnBreed.addEventListener("click", (e) => {
-  alert("test");
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].value === "") {
-      e.preventDefault();
-      input[i].classList.add("border-red-500");
-    } else {
-      input[i].classList.remove("border-red-500");
-    }
-  }
-});
+// btnTrait.addEventListener("click", (e) => {
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].value === "") {
+//       e.preventDefault();
+//       input[i].classList.add("border-red-500");
+//     } else {
+//       input[i].classList.remove("border-red-500");
+//     }
+//   }
+// });
